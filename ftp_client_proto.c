708a01ae6fdc334e5ed00581a5494d1e42a074c7
xiuyi2017 <873604424@qq.com>
2017-06-26 22:35:15 -0700

    add comment for proto

--- ftp_client_proto.c
+++ ftp_client_proto.c
@@ -1,3 +1,13 @@
+/*-----------------------------------------------------------------------*
+ * Introduction:                                                         *
+ *                                                                       *
+ *   Realize the transmit protocol of ftp client.                        *
+ *   It contains:                                                        *
+ *   --> 1. request command to ftp server;                               * 
+ *   --> 2. handle the reply code from ftp server.                       *
+ *                                                                       *
+ * ----------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
